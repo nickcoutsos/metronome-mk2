416cac6e5dad5eef3d42f420cbbddda172b98e13
Nick Coutsos <nick@coutsos.com>
2013-03-17 21:41:43 -0400

    Tweaked some class names and styling. Added more options to mousehold.

--- puredom.mousehold.js
+++ puredom.mousehold.js
@@ -3,6 +3,9 @@ puredom.addNodeSelectionPlugin('mousehold', function(options) {
     var options = options || {};
     options.timeout = options.timeout || 0.95;
     options.interval = options.interval || 0.6;
+    options.eventName = options.eventName || 'mouseheld';
+    options.preventDefault = options.preventDefault || false;
+    options.fireImmediately = options.fireImmediately || false;
     
     puredom(this).each(function(selected) {
         
@@ -15,7 +18,7 @@ puredom.addNodeSelectionPlugin('mousehold', function(options) {
                 interval = setInterval(repeat, options.interval * 1000);
             }
             
-            selected.fireEvent('mouseheld', down);
+            selected.fireEvent(options.eventName, down);
         }
         
         function clear() {
@@ -29,9 +32,18 @@ puredom.addNodeSelectionPlugin('mousehold', function(options) {
             }
         }
         
-        puredom(selected).on('mousedown', function(e) {
-            timeout = setTimeout(repeat, options.timeout * 1000);
-        }).on('mouseleave', clear);
+        puredom(selected)
+            .on('mousedown', function(e) {
+                timeout = setTimeout(repeat, options.timeout * 1000);
+                if (options.fireImmediately) {
+                    selected.fireEvent(options.eventName, e);
+                }
+                if (options.preventDefault) {
+                    e.preventDefault();
+                }
+            })
+            .on('mouseleave', clear);
+        
         puredom(window).on('mouseup', clear);
     });
 });
